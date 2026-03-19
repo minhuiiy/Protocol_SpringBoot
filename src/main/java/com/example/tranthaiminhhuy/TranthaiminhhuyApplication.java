55d556beb4d94d72c5108cceb8891db761305e04
minhuyi <minhhuy5929@gmail.com>
2026-03-19 10:54:40 +0700

    init project

---
+++ src/main/java/com/example/tranthaiminhhuy/TranthaiminhhuyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tranthaiminhhuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranthaiminhhuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranthaiminhhuyApplication.class, args);
+	}
+
+}
